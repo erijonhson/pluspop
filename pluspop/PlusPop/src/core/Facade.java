bfebdce79925e70a38d2235ae7b3dd304627375f
erijonhson <erijonhson.os@gmail.com>
2015-08-08 16:19:36 -0300

    Esboço do Projeto.

---
+++ pluspop/PlusPop/src/core/Facade.java
@@ -0,0 +1,12 @@
+package core;
+
+import easyaccept.EasyAccept;
+
+public class Facade {
+
+	public static void main(String[] args) {
+		args = new String[] { "core.Facade", "resources/Scripts de Teste/usecase_1.txt" };
+		EasyAccept.main(args);
+	}
+
+}
